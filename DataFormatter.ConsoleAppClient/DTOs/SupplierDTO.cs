1bc77718b37371dd384ba2035b7db371b556960b
randelramirez <randelramirez1@gmail.com>
2016-06-15 00:38:21 +0800

    cleanup

--- DataFormatter.ConsoleAppClient/DTOs/SupplierDTO.cs
+++ DataFormatter.ConsoleAppClient/DTOs/SupplierDTO.cs
@@ -14,7 +14,10 @@ namespace DataFormatter.ConsoleAppClient.DTOs
         // we can add XmlIgnore attribute below or simply in the client code omit this property when mapping to this dto
         // ex. data.Select(p = new DTO{ Id = p.Id,  //Products = p.Products});
 
- 
-        public  List<Product> Products { get; set; }
+        // might need to add script ignore, or xml ignore on properties that won't be serialized since 
+        // it doesn't make sense to actually add {property: null} on the actual serialized string result.
+        // but as stated above it might depend on the use case because what if we are serializing the SupplierDTO and we wanted to include its products
+        // currently we are serializing Products entity on the client(console app)
+        public List<Product> Products { get; set; }
     }
 }
--- DataFormatter.ConsoleAppClient/Program.cs
+++ DataFormatter.ConsoleAppClient/Program.cs
@@ -1,13 +1,10 @@
-﻿using DataFormatter.Core;
+﻿using DataFormatter.ConsoleAppClient.DTOs;
 using DataFormatter.Infrastructure;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Linq;
 using System.Xml.Serialization;
-using DataFormatter.ConsoleAppClient.DTOs;
 
 namespace DataFormatter.ConsoleAppClient
 {
@@ -15,14 +12,31 @@ namespace DataFormatter.ConsoleAppClient
     {
         static void Main(string[] args)
         {
+            #region Lazy Loading Note
+
+            /*
+             Turn lazy loading off for serialization
+             
+             Lazy loading and serialization don’t mix well, and if you aren’t careful you can end up querying for your entire database 
+             just because lazy loading is enabled.Most serializers work by accessing each property on an instance of a type.
+             Property access triggers lazy loading, so more entities get serialized.
+             On those entities properties are accessed, and even more entities are loaded.
+             It’s a good practice to turn lazy loading off before you serialize an entity.The following sections show how to do this.
+            */
+            //https://msdn.microsoft.com/en-us/data/jj574232.aspx
+            #endregion
+
             var dataContext = new DataContext();
             //dataContext.Configuration.LazyLoadingEnabled = false;
             //dataContext.Configuration.ProxyCreationEnabled = false;
 
+            // not working
+            //var json1 = SerializeJSON1(dataContext);
+            //Console.WriteLine(json1);
 
-            var json1 = SerializeJSON1(dataContext);
-            Console.WriteLine(json1);
-          
+            var json2 = SerializeJSON2(dataContext);
+            Console.WriteLine(json2);
+
             Console.ReadKey();
 
         }
@@ -30,10 +44,19 @@ namespace DataFormatter.ConsoleAppClient
         // EFContext lazy loading enabled
         // proxies enabled
         // using projection, anonymous object, no DTO.
+        // so far works when lazy loading = false
         static string SerializeJSON1(DataContext context)
         {
             var data = context.Products.Include(p => p.Supplier);
             var toJson = data.Select(p => new { Id = p.Id, Price = p.Price, Supplier = p.Supplier }).ToList();
+            var jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(toJson);
+            return jsonResult;
+        }
+
+        static string SerializeJSON2(DataContext context)
+        {
+            var data = context.Products.Include(p => p.Supplier);
+            var toJson = data.Select(p => new ProductDTO { Id = p.Id, Name = p.Name, Price = p.Price, Supplier = new SupplierDTO { Id = p.Supplier.Id, Name = p.Supplier.Name, Address = p.Supplier.Address } });
             var jsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(toJson);
             return jsonResult;
         }
@@ -64,6 +87,4 @@ namespace DataFormatter.ConsoleAppClient
             return xmlResult;
         }
     }
-
-
 }
